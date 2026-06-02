e2af66ac4d9c6bdad61c80d78eb3cdbc9280bd71
Souptik800700 <souptikkhatua@gmail.com>
2026-06-02 09:01:54 +0530

    Add Chapter 2 folder

---
+++ Chapter 2 PS/05_problem5.py
@@ -0,0 +1,5 @@
+a = int(input("Enter number 1 "))
+b = int(input("Enter number 2"))
+c = (a+b)/2
+
+print(c)
--- practice set/ps.py
+++
@@ -1,9 +0,0 @@
-import os
-
-path = "."   # Current directory
-
-contents = os.listdir(path)
-
-print("Contents of the directory:")
-for item in contents:
-    print(item)
